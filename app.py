883ca90e47050773759142375d26c8fb6baa886b
user <kokhaniuk.nataliia@chnu.edu.ua>
2025-05-29 19:53:23 +0300

    44

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ def hello():
 
 '''
 
+'''
 from flask import Flask, render_template, request, redirect, send_from_directory
 import os
 
@@ -47,3 +48,61 @@ def page5():
 
 if __name__ == "__main__":
     app.run(debug=True)
+'''
+
+from flask import Flask, request, render_template, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
+    'DATABASE_URL',
+    'postgresql://database_fyzl_user:your_password@dpg-d0s8ukadbo4c73bfcicg-a/database_fyzl'
+)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class Item(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+
+    def __repr__(self):
+        return f'<Item {self.id}: {self.content}>'
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/3')
+def quotas():
+    return render_template('3.html')
+
+
+@app.route('/4', methods=['GET'])
+def show_items():
+    items = Item.query.all()
+    return render_template('4.html', items=items)
+
+@app.route('/create', methods=['POST'])
+def create_item():
+    content = request.form.get('item')
+    if content:
+        new_item = Item(content=content)
+        db.session.add(new_item)
+        db.session.commit()
+    return redirect('/4')
+
+@app.route('/delete/<int:item_id>')
+def delete_item(item_id):
+    item = Item.query.get_or_404(item_id)
+    db.session.delete(item)
+    db.session.commit()
+    return redirect('/4')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
