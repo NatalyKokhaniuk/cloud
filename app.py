896baee3be7caed32052c7ca1f9dc117cf25a347
user <kokhaniuk.nataliia@chnu.edu.ua>
2025-05-29 20:12:19 +0300

    44

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-'''from flask import Flask
+from flask import Flask
 
 app = Flask(__name__)
 
@@ -6,51 +6,10 @@ app = Flask(__name__)
 def hello():
     return "Привіт з на Render! Це Коханюк Наталя"
 
-'''
 
-'''
-from flask import Flask, render_template, request, redirect, send_from_directory
-import os
 
-app = Flask(__name__)
 
-items = []
-
-@app.route("/")
-def index():
-    return render_template("index.html", items=items)
-
-@app.route("/create", methods=["POST"])
-def create():
-    item = request.form.get("item")
-    if item:
-        items.append(item)
-    return redirect("/")
-
-@app.route("/delete/<int:index>")
-def delete(index):
-    if 0 <= index < len(items):
-        items.pop(index)
-    return redirect("/")
-
-@app.route("/3.html")
-def page3():
-    return render_template("3.html")
-
-@app.route("/4")
-def page4():
-    items = Item.query.all()
-    return render_template("4.html", items=items)
-
-@app.route("/5.html")
-def page5():
-    return render_template("5.html")
-
-if __name__ == "__main__":
-    app.run(debug=True)
-'''
-
-from flask import Flask, request, render_template, redirect
+'''from flask import Flask, request, render_template, redirect
 from flask_sqlalchemy import SQLAlchemy
 import os
 
@@ -120,4 +79,4 @@ def delete_db_item(item_id):
 with app.app_context():
     db.create_all()
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True)'''
