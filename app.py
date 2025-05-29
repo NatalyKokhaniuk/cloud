1bf237d18fd3c482302a372c337853b484df25ac
user <kokhaniuk.nataliia@chnu.edu.ua>
2025-05-29 20:26:10 +0300

    44

--- app.py
+++ app.py
@@ -1,71 +1,40 @@
-from flask import Flask, request, render_template, redirect
 
+from flask import Flask, render_template, request, redirect, send_from_directory
 import os
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
-    'DATABASE_URL',
-    'postgresql://database_fyzl_user:fT4Up7uMKWAgcjhJtbFNbyUIcaxAQMD8@dpg-d0s8ukadbo4c73bfcicg-a/database_fyzl'
-)
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+items = []
 
-db = SQLAlchemy(app)
-
-class Item(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.String(200), nullable=False)
-
-    def __repr__(self):
-        return f'<Item {self.id}: {self.content}>'
-
-
-local_items = []
-
-
-@app.route('/')
+@app.route("/")
 def index():
-    return render_template('index.html', items=local_items)
+    return render_template("index.html", items=items)
 
-@app.route('/create', methods=['POST'])
-def create_local_item():
+@app.route("/create", methods=["POST"])
+def create():
     item = request.form.get("item")
     if item:
-        local_items.append(item)
-    return redirect('/')
+        items.append(item)
+    return redirect("/")
 
-@app.route('/delete/<int:index>')
-def delete_local_item(index):
-    if 0 <= index < len(local_items):
-        local_items.pop(index)
-    return redirect('/')
+@app.route("/delete/<int:index>")
+def delete(index):
+    if 0 <= index < len(items):
+        items.pop(index)
+    return redirect("/")
 
-@app.route('/3')
-def quotas():
-    return render_template('3.html')
+@app.route("/3.html")
+def page3():
+    return render_template("3.html")
 
-@app.route('/4')
-def show_items():
-    db_items = Item.query.all()
-    return render_template('4.html', items=db_items)
+@app.route("/4")
+def page4():
+    items = Item.query.all()
+    return render_template("4.html", items=items)
 
-@app.route('/db/create', methods=['POST'])
-def create_db_item():
-    content = request.form.get('item')
-    if content:
-        new_item = Item(content=content)
-        db.session.add(new_item)
-        db.session.commit()
-    return redirect('/4')
+@app.route("/5.html")
+def page5():
+    return render_template("5.html")
 
-@app.route('/db/delete/<int:item_id>')
-def delete_db_item(item_id):
-    item = Item.query.get_or_404(item_id)
-    db.session.delete(item)
-    db.session.commit()
-    return redirect('/4')
-
-with app.app_context():
-    db.create_all()
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
