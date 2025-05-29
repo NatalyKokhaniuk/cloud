49c005fd8e3c186971bb4afd6a5d9793498577f0
user <kokhaniuk.nataliia@chnu.edu.ua>
2025-05-29 19:15:33 +0300

    2

--- app.py
+++ app.py
@@ -1,7 +1,31 @@
-from flask import Flask
+'''from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Привіт з на Render! Це Коханюк Наталя"
+
+'''
+
+from flask import Flask, request, redirect, render_template
+
+app = Flask(__name__)
+items = []
+
+@app.route("/")
+def index():
+    return render_template("index.html", items=items)
+
+@app.route("/create", methods=["POST"])
+def create():
+    item = request.form["item"]
+    if item:
+        items.append(item)
+    return redirect("/")
+
+@app.route("/delete/<int:item_id>")
+def delete(item_id):
+    if 0 <= item_id < len(items):
+        items.pop(item_id)
+    return redirect("/")
